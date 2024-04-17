0f5112066d0b4806415dee22a80c9ea22c203a97
asimkoul <ahmadasim080gmail.com>
2024-04-17 12:44:32 +0530

    middlewares

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http=require("http")
 const express=require("express")
 const app=express()
-
-
-const server=http.createServer(app);
-
-server.listen(3000)
+app.use((req,res,next)=>{
+    console.log("in the middle ware")
+    next()
+})
+app.use((req,res,next)=>{
+    console.log("in another middle ware")
+    res.send('<h1>hello from express</h1>')
+})
+app.listen(3000)
