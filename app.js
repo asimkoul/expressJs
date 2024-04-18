90378b12b430389fc05c1510de154c251dd9b5c6
asimkoul <ahmadasim080gmail.com>
2024-04-18 15:58:57 +0530

    bodyParser

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http=require("http")
 const express=require("express")
+const bodyParser=require('body-parser')
 const app=express()
-app.use((req,res,next)=>{
-    console.log("in the middle ware")
-    next()
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product', (req,res,next)=>{
+    res.send('<form action="/product" method="POST">Name<input type="text" name="title">Size<input type="number" name="size"><button type="submit">Add Product</button></form>')
 })
-app.use((req,res,next)=>{
-    console.log("in another middle ware")
+app.post('/product', (req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/', (req,res,next)=>{
     res.send('<h1>hello from express</h1>')
 })
+
 app.listen(3000)
